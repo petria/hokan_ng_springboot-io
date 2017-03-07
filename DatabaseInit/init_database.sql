becc0093dbb7d48b5efa0581b374e924d03ec6a2
Petri Airio <petri.j.airio@gmail.com>
2017-03-07 11:45:13 +0200

    Database create scripts for MariaDB

---
+++ DatabaseInit/init_database.sql
@@ -0,0 +1,11 @@
+#
+# Create or reset mariadb database hokan_ng_springboot
+#
+
+DROP DATABASE  IF EXISTS hokan_ng_springboot;
+
+CREATE DATABASE `hokan_ng_springboot` CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON `hokan_ng_springboot`.* TO `hokan_ng`@`localhost` IDENTIFIED BY 'hokan_ng';
+GRANT ALL ON `hokan_ng_springboot`.* TO `hokan_ng`@`%` IDENTIFIED BY 'hokan_ng';
+
+FLUSH PRIVILEGES;
